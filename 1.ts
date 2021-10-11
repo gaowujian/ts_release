49311c8256a1b92d0314ecf53e9f111d652bb98f
tony <806003097@qq.com>
2021-10-11 17:30:13 +0800

    update

--- 1.ts
+++ 1.ts
@@ -1,44 +1,21 @@
-// async function test(): Promise<string> {
-//   return await new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       resolve("success");
-//     }, 1000);
-//   });
-// }
-// test();
-
-// function assign<T extends U, U>(target: T, source: U): T {
-//   for (let id in source) {
-//     target[id] = source[id];
-//   }
-//   return target;
-// }
-// let x = { a: 1, b: 2, c: 3, d: 4 };
-// assign(x, { b: 10, d: 20 });
-// // assign(x, { e: 0 }); // Error
-
-// let p: { a: number; b: number } extends { a: number } ? string : boolean;
-// var a = [1, 2, 3];
-// for (var x in a) {
-//   // Type of x is implicitly string
-//   var obj = a[x]; // Type of obj is MyObject
-//   console.log("obj:", obj);
-// }
-
-// Compiled with --strictNullChecks
-// let x: number;
-// let y: number | null;
-// let z: number | undefined;
-// x; // Error, reference not preceded by assignment
-// y; // Error, reference not preceded by assignment
-// z; // Ok
-// x = 1;
-// y = null;
-// x; // Ok
-// y; // Ok
-
-let x: null;
-let y: undefined;
-
-// console.log(x);
-console.log(y);
+class A {}
+class B extends A {}
+class C extends A {}
+class D extends A {
+  c: string = "";
+}
+class E extends D {}
+let x1 = !true ? new A() : new B(); // A
+let x2 = !true ? new B() : new C(); // B | C (previously B)
+let x3 = !true ? new C() : new D(); // C | D (previously C)
+let a1 = [new A(), new B(), new C(), new D(), new E()]; // A[]
+let a2 = [new B(), new C(), new D(), new E()]; // (B | C | D)[] (previously B[])
+function f1(x: B | C | D) {
+  if (x instanceof B) {
+    x; // B (previously B | D)
+  } else if (x instanceof C) {
+    x; // C
+  } else {
+    x; // D (previously never)
+  }
+}
--- 2.ts
+++ 2.ts
@@ -1,6 +1,8 @@
-import calc from "./ScientificCalculator";
-let v = new calc(0.5)
-  .square()
-  .divide(2)
-  .sin() // Error: 'BasicCalculator' has no 'sin' method.
-  .currentValue();
+// import calc from "./ScientificCalculator";
+// let v = new calc(0.5)
+//   .square()
+//   .divide(2)
+//   .sin() // Error: 'BasicCalculator' has no 'sin' method.
+//   .currentValue();
+
+export default 5;
